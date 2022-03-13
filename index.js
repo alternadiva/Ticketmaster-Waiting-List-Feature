dd7f6197ca50c3cab3b67f24cadb742b05e94d39
alternadiva <asomeonelse@gmail.com>
2022-03-13 23:26:03 +0000

    get form entries

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+// Get form entries
+
+const form = document.getElementById("waiting-list-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+  return data;
+});
