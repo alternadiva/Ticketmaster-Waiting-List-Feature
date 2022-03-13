f9dac099051ce07f3aa4314144b922a0a7edf506
alternadiva <asomeonelse@gmail.com>
2022-03-13 23:36:57 +0000

    add waiting list array of objects

--- index.js
+++ index.js
@@ -2,10 +2,22 @@
 
 const form = document.getElementById("waiting-list-form");
 
+// Add waiting list array of objects for comparison
+
+let waitingList = [
+  {
+    mobile: "+440123456789",
+    email: "example@example.com",
+  },
+  {
+    mobile: "+449876543210",
+    email: "test@test.com",
+  },
+];
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const formData = new FormData(form);
   const data = Object.fromEntries(formData);
-  return data;
 });
