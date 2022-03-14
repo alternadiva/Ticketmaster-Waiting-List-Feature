ca153f3b313c27052ad2871177e84b9db0c7339a
alternadiva <asomeonelse@gmail.com>
2022-03-14 00:08:04 +0000

    compare form data with waiting list data

--- index.js
+++ index.js
@@ -10,14 +10,29 @@ let waitingList = [
     email: "example@example.com",
   },
   {
-    mobile: "+449876543210",
+    mobile: "+449876543211",
     email: "test@test.com",
   },
 ];
+
+// Compare form data with waiting list data
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const formData = new FormData(form);
-  const data = Object.fromEntries(formData);
+  const userData = Object.fromEntries(formData);
+
+  let onTheList = false;
+
+  for (let i = 0; i < waitingList.length; i++) {
+    if (
+      waitingList[i].mobile === userData.mobile ||
+      waitingList[i].email === userData.email
+    ) {
+      onTheList = true;
+      break;
+    }
+  }
+  return onTheList;
 });
